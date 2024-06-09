25fd3c18c5ccd2b0d419984702204f06eadc825a
Heryd <silla0408@hotmail.com>
2024-06-09 00:43:09 -0500

    create methods to save, load and validate user credentials.

--- app/src/main/java/com/dam/MediTrackYou/LoginActivity.java
+++ app/src/main/java/com/dam/MediTrackYou/LoginActivity.java
@@ -1,5 +1,18 @@
 package com.dam.MediTrackYou;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,6 +22,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText user;
+    private EditText password;
+    private final String userVault = "Grupo4_DAM";
+    private final String passwordVault = "desarrollomovil";
+
+    private CheckBox mantenerSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +39,44 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        user = findViewById(R.id.txt_user);
+        password = findViewById(R.id.txt_password);
+
+        mantenerSesion = findViewById(R.id.chkMantenerSesion);
+        cargarCredenciales();
     }
+
+    public String getPasswordVault() {
+        return passwordVault;
+    }
+
+    public String getUserVault() {
+        return userVault;
+    }
+
+    private void cargarCredenciales() {
+        SharedPreferences shpLogin = getSharedPreferences("CredencialAcceso", Context.MODE_PRIVATE);
+        String user_tmp = shpLogin.getString("userSHP", "");
+        String password_tmp = shpLogin.getString("passwordSHP", "");
+        user.setText(user_tmp);
+        password.setText(password_tmp);
+        if (user_tmp != "") {
+            mantenerSesion.setChecked(true);
+        } else {
+            mantenerSesion.setChecked(false);
+        }
+    }
+
+    private void guardarCredenciales(String user, String password) {
+        SharedPreferences shpLogin = getSharedPreferences("CredencialAcceso", Context.MODE_PRIVATE);
+        SharedPreferences.Editor shpLoginEdit = shpLogin.edit();
+        shpLoginEdit.putString("userSHP", user);
+        shpLoginEdit.putString("passwordSHP", password);
+        shpLoginEdit.commit();
+    }
+
+    protected Boolean validateCredentials(Editable userInput, Editable passwordInput) {
+        return (userInput.toString().trim().equals(getUserVault()) && passwordInput.toString().trim().equals(getPasswordVault()));
+    }
+
 }
