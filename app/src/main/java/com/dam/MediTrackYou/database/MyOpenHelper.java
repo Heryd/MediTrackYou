da4a5c27c057df0426654b301c7efcdd3cfa7819
Heryd <silla0408@hotmail.com>
2024-06-09 13:54:18 -0500

    create the MyOpenHelper class for the CRUD of records with SQLite.

---
+++ app/src/main/java/com/dam/MediTrackYou/database/MyOpenHelper.java
@@ -0,0 +1,39 @@
+package com.dam.MediTrackYou.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyOpenHelper extends SQLiteOpenHelper {
+
+    public static final String usuario = "CREATE TABLE user ( " +
+            "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "dni TEXT," +
+            "age INTEGER," +
+            "name TEXT," +
+            "lastname TEXT," +
+            "birth_date TEXT," +
+            "gender TEXT," +
+            "nationality TEXT," +
+            "email TEXT," +
+            "phone TEXT," +
+            "password TEXT)";
+
+    public static final String dbName = "meditrackyou.sqlite";
+    public static final int dbversion = 1;
+
+    public MyOpenHelper(Context context) {
+        super(context, dbName, null, dbversion);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(usuario);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+}
