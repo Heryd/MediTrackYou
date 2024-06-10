b8c7c6a0aee32e1efa7fbf114016b0cd0c515480
Heryd <silla0408@hotmail.com>
2024-06-09 23:16:18 -0500

    override the onOptionsItemSelected method.

--- app/src/main/java/com/dam/MediTrackYou/MainActivity.kt
+++ app/src/main/java/com/dam/MediTrackYou/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-    fun onOptionsItemSelected(item: MenuItem?): Boolean {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item?.itemId) {
             R.id.itemAcercade -> {
                 val ventanaAcercade = Intent(this, AcercadeActivity::class.java)
