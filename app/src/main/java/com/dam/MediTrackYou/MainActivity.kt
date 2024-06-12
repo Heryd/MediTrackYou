77c3a15c12c8fb081cf01780f1d2c508dd80ea90
Heryd <silla0408@hotmail.com>
2024-06-12 01:31:46 -0500

    added compose functions to MainActivity class

--- app/src/main/java/com/dam/MediTrackYou/MainActivity.kt
+++ app/src/main/java/com/dam/MediTrackYou/MainActivity.kt
@@ -5,33 +5,73 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dam.MediTrackYou.ui.theme.MediTrackYouTheme
 import com.dam.MediTrackYou.view.about.AcercadeActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContent {
+            MediTrackYouTheme {
+                MainScreen()
+            }
         }
+    }
+
+    @Composable
+    fun MainScreen() {
+        val context = LocalContext.current
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            MainText()
+        }
+    }
+
+    @Composable
+    fun MainText() {
+        Text(
+            text = "Bienvenido Usuario! :D",
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.onBackground
+        )
+    }
+
+    @Preview()
+    @Composable
+    fun PreviewMainScreen() {
+        MainScreen()
     }
 
     //Tarea 1 - CREACION DE MENU y LLAMADAS A OTRAS PANTALLAS
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        var menuPrincipal: MenuInflater = menuInflater
+        val menuPrincipal: MenuInflater = menuInflater
         menuPrincipal.inflate(R.menu.menuprincipal, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item?.itemId) {
+        when (item.itemId) {
             R.id.itemAcercade -> {
                 val ventanaAcercade = Intent(this, AcercadeActivity::class.java)
                 startActivity(ventanaAcercade)
