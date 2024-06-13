ea35229bb07772a4db88d500da27a4244cac3eea
Heryd <silla0408@hotmail.com>
2024-06-12 22:21:32 -0500

    changed the name of the MyOpenHelper class to BDMediTrackYou added function to register a new user in the firebase DB added function to check if the user exists in the firebase DB

--- app/src/main/java/com/dam/MediTrackYou/model/database/BDMediTrackYou.kt
+++ app/src/main/java/com/dam/MediTrackYou/model/database/BDMediTrackYou.kt
@@ -1,38 +1,19 @@
-package com.dam.MediTrackYou.model.database;
+package com.dam.MediTrackYou.model.database
 
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context
 
-public class MyOpenHelper extends SQLiteOpenHelper {
+class BDMediTrackYou(context: Context?) {
+    //Registrar al nuevo usuario en la Base de Datos de Firebase
+    fun writeNewUser(userId: String) {
 
-    public static final String usuario = "CREATE TABLE user ( " +
-            "id_user INTEGER PRIMARY KEY AUTOINCREMENT, " +
-            "dni TEXT," +
-            "age INTEGER," +
-            "name TEXT," +
-            "lastname TEXT," +
-            "birth_date TEXT," +
-            "gender TEXT," +
-            "nationality TEXT," +
-            "email TEXT," +
-            "phone TEXT," +
-            "password TEXT)";
-
-    public static final String dbName = "meditrackyou.sqlite";
-    public static final int dbversion = 1;
-
-    public MyOpenHelper(Context context) {
-        super(context, dbName, null, dbversion);
     }
 
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL(usuario);
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    //Verificar si el usuario existe en la Base de Datos
+    fun findUserExists(dni: String, username: String): Boolean {
+        //when(){
+        //
+        //      }
+        return true
     }
 
 }
