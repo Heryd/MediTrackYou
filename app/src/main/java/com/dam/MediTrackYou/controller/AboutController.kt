51c6246fc9c65743da1039b124c55b5952ea6d8c
Heryd <silla0408@hotmail.com>
2024-06-14 06:27:23 -0500

    change the function name to Back fixed logic error in the function that led to login instead of ending the activity

--- app/src/main/java/com/dam/MediTrackYou/controller/AboutController.kt
+++ app/src/main/java/com/dam/MediTrackYou/controller/AboutController.kt
@@ -2,11 +2,13 @@ package com.dam.MediTrackYou.controller
 
 import android.content.Context
 import android.content.Intent
+import androidx.activity.ComponentActivity
 import com.dam.MediTrackYou.view.auth.login.LoginActivity
 
 class AboutController {
-    fun BackToLogin(context: Context) {
-        val loginActivity = Intent(context, LoginActivity::class.java)
-        context.startActivity(loginActivity, null)
+    fun Back(context: Context) {
+        if (context is ComponentActivity) {
+            context.finish()
+        }
     }
 }
