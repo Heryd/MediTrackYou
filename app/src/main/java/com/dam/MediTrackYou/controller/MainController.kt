3c10786b65fc4ae218186f1829e8ee4d3fe06045
Heryd <silla0408@hotmail.com>
2024-06-14 07:01:50 -0500

    added new MainController class

---
+++ app/src/main/java/com/dam/MediTrackYou/controller/MainController.kt
@@ -0,0 +1,18 @@
+package com.dam.MediTrackYou.controller
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.CalendarMonth
+import androidx.compose.material.icons.outlined.DeviceUnknown
+import androidx.compose.material.icons.outlined.Flag
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import com.dam.MediTrackYou.R
+
+class MainController {
+}
